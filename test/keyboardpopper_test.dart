c5240c8072877dcf53ae59294b64c888d77bd9fe
ashleshpandey <ashlesh.pandey@prixa.org>
2023-10-18 13:50:40 +0545

    feat: testWidgets for KeyboardPopper

--- test/keyboardpopper_test.dart
+++ test/keyboardpopper_test.dart
@@ -1,12 +1,66 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:keyboardpopper/keyboardpopper.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  testWidgets('KeyboardPopper dismisses keyboard on tap',
+      (WidgetTester tester) async {
+    // Build a test widget tree with KeyboardPopper.
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: KeyboardPopper(
+          child: Scaffold(
+            body: Center(
+              child: TextField(),
+            ),
+          ),
+        ),
+      ),
+    );
+
+    // Tap on the text field to bring up the keyboard.
+    await tester.tap(find.byType(TextField));
+    await tester.pump();
+
+    // Expect that the keyboard is shown.
+    expect(tester.testTextInput.isVisible, isTrue);
+
+    // Tap outside of the text field to dismiss the keyboard.
+    await tester.tap(find.byType(Scaffold));
+    await tester.pump();
+
+    // Expect that the keyboard is hidden.
+    expect(tester.testTextInput.isVisible, isFalse);
+  });
+
+  testWidgets('KeyboardPopper does not dismiss keyboard on tap inside button',
+      (WidgetTester tester) async {
+    // Build a test widget tree with a button inside KeyboardPopper.
+    await tester.pumpWidget(
+      MaterialApp(
+        home: KeyboardPopper(
+          child: Scaffold(
+            body: Center(
+              child: Column(
+                children: [
+                  const TextField(),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Button'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+
+    // Tap on the button inside the KeyboardPopper.
+    await tester.tap(find.text('Button'));
+    await tester.pump();
+
+    // Expect that the keyboard is still shown.
+    expect(tester.testTextInput.isVisible, isTrue);
   });
 }
